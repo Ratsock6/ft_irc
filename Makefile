9514139fef4ab9e3f17c518650362e7849bc8a48
DailyWind <140258565+DailyWind00@users.noreply.github.com>
2024-07-11 11:33:52 +0200

    ft_irc setup

---
+++ Makefile
@@ -0,0 +1,59 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: mgallais <mgallais@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/07/11 11:27:37 by mgallais          #+#    #+#              #
+#    Updated: 2024/07/11 11:29:45 by mgallais         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME    = ircserv
+CC      = c++
+RM      = rm -f
+CFLAGS += -g -Wall -Wextra -Werror -std=c++98
+INCLUDE = -I include -I include/classes -I lib/DailyLib
+DEBUG   = 0
+
+SRC		= src/main.cpp #
+
+OBJ		= $(SRC:.cpp=.o)
+
+all:
+	@printf "\033[1;36m\nStarting ircserv compilation\n\e[0m"
+	@make -s redirect
+
+redirect: $(NAME)
+
+$(NAME): $(OBJ)
+	@$(CC) $(CFLAGS) -o $(NAME) $(OBJ)
+	@printf "\033[1;92m >>> Ircserv compiled                                                     \n\n\033[0m"
+
+%.o: %.cpp
+	@printf "\033[0;33mCompilation de %-33.33s\r\e[0m" $@
+	@if [ $(DEBUG) -eq 1 ]; then $(CC) $(CFLAGS) $(INCLUDE) -c $< -o $@ -D DEBUG=1; else $(CC) $(CFLAGS) $(INCLUDE) -c $< -o $@; fi
+
+clean:
+	@$(RM) $(OBJ)
+	@printf "\033[1;31m- Object files deleted\n\033[0m"
+
+fclean: clean	
+	@$(RM) $(NAME)
+	@printf "\033[1;31m- Executable file deleted\n\033[0m"
+
+re: fclean all
+
+test:
+	@make -s re
+	@make -s clean
+	@clear
+	@echo "\033[1;92mDONE !"
+
+debug: fclean
+	@make -s DEBUG=1 all
+	@make clean
+	@clear
+
+.PHONY: all redirect clean fclean re test debug
---
+++ lib/DailyLib/dailylib.h
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dailylib.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgallais <mgallais@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/11 09:59:32 by mgallais          #+#    #+#             */
+/*   Updated: 2024/07/11 11:31:45 by mgallais         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DAILYLIB_H
+# define DAILYLIB_H
+
+/*###########################################################################*/
+/*                                  Defines                                  */
+/*###########################################################################*/
+// Custom defines
+# define WHITESPACES "\t\n\v\f\r "
+
+/*###########################################################################*/
+/*                                 Includes                                  */
+/*###########################################################################*/
+// Standard includes
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <stdint.h>
+# include <stdbool.h>
+# include <string.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <math.h>
+
+/*###########################################################################*/
+/*                                Structures                                 */
+/*###########################################################################*/
+// Structure for 2 integers
+typedef struct s_2int
+{
+	int		x;
+	int		y;
+}	t_2int;
+
+// Structure for 3 integers
+typedef struct s_3int
+{
+	int		x;
+	int		y;
+	int		z;
+}	t_3int;
+
+// Structure for 4 integers
+typedef struct s_4int
+{
+	int		r;
+	int		g;
+	int		b;
+	int		l;
+}	t_4int;
+
+// Structure for 2 floats
+typedef struct s_2float
+{
+	float	x;
+	float	y;
+}	t_2float;
+
+// Structure for 3 floats
+typedef struct s_3float
+{
+	float	x;
+	float	y;
+	float	z;
+}	t_3float;
+
+// Structure for 4 floats
+typedef struct s_4float
+{
+	float	x;
+	float	y;
+	float	z;
+	float	w;
+}	t_4float;
+
+// Structure for 3 chars
+typedef struct s_RBG
+{
+	char	r;
+	char	g;
+	char	b;
+}	t_RGB;
+
+// Structure for 4 chars
+typedef struct s_RBGA
+{
+	char	r;
+	char	g;
+	char	b;
+	char	a;
+}	t_RGBA;
+
+#endif
