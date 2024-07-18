9c6cecde2aa517b0a85dc3067267bd7b66b59c65
DailyWind <140258565+DailyWind00@users.noreply.github.com>
2024-07-18 14:40:25 +0200

    modified dailylib

--- include/main.hpp
+++ include/main.hpp
@@ -6,7 +6,7 @@
 /*   By: mgallais <mgallais@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 09:55:33 by mgallais          #+#    #+#             */
-/*   Updated: 2024/07/18 10:52:02 by mgallais         ###   ########.fr       */
+/*   Updated: 2024/07/18 14:33:24 by mgallais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,36 +22,29 @@
 /*###########################################################################*/
 /*                                  Includes                                 */
 /*###########################################################################*/
-// Standard libraries
-#include <iostream>      // For standard input/output functions (std::cout, std::cin, std::cerr)
-#include <algorithm>     // For functions std::min, std::max, std::sort, std::find, std::count, std::replace, std::remove
-#include <cstring>       // For string manipulation functions (strlen, strcpy, strcmp, ...)
-#include <sys/types.h>   // For socket data types
-#include <sys/socket.h>  // For socket functions: socket, bind, connect, listen, accept, setsockopt
-#include <sys/time.h>    // For struct timeval used in select()
-#include <unistd.h>      // For functions close, lseek, fstat
-#include <netinet/in.h>  // For functions htons, htonl, ntohs, ntohl and data structures for addresses
-#include <arpa/inet.h>   // For functions inet_addr, inet_ntoa
-#include <netdb.h>       // For functions getprotobyname, gethostbyname, getaddrinfo, freeaddrinfo
-#include <csignal>       // For functions signal, sigaction
-#include <fcntl.h>       // For functions fcntl
-#include <poll.h>        // For function poll
-#include <sys/stat.h>    // For function fstat
-#include <cstdio>        // For standard input/output functions if needed (e.g. printf)
-#include <cstdlib>       // For type conversion functions if needed (e.g. atoi, atof)
-#include <ctime>         // For time functions (time, clock, difftime, mktime, ...)
-#include <sstream>       // For string stream functions (std::stringstream)
+// IRC needed libraries
+# include <sys/types.h>   // For socket data types
+# include <sys/socket.h>  // For socket functions: socket, bind, connect, listen, accept, setsockopt
+# include <sys/time.h>    // For struct timeval used in select()
+# include <netinet/in.h>  // For functions htons, htonl, ntohs, ntohl and data structures for addresses
+# include <arpa/inet.h>   // For functions inet_addr, inet_ntoa
+# include <netdb.h>       // For functions getprotobyname, gethostbyname, getaddrinfo, freeaddrinfo
+# include <fcntl.h>       // For functions fcntl
+# include <poll.h>        // For function poll
+# include <sys/stat.h>    // For function fstat
 
 // Custom libraries
-#include "color.h"       // For color definitions
-#include "dailylib.h"	 // For custom defines and structures
+# include "color.h"       // For color definitions
+
+# define DAILYLIB_DEF 0	 // Defines not needed
+# include "dailylib.h"	 // For custom defines and structures
 
 /*###########################################################################*/
 /*                            Structures & Classes                           */
 /*###########################################################################*/
 // Classes
-#include "Server.hpp"
-#include "Client.hpp"
+# include "Server.hpp"
+# include "Client.hpp"
 
 // Structures
 
