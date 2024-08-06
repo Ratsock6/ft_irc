1291ba42a1da9e8c03fec6a9afdf909db7f15745
DailyWind <140258565+DailyWind00@users.noreply.github.com>
2024-08-06 15:48:34 +0200

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mgallais <mgallais@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 09:54:06 by mgallais          #+#    #+#             */
-/*   Updated: 2024/07/31 10:35:26 by mgallais         ###   ########.fr       */
+/*   Updated: 2024/08/06 15:48:15 by mgallais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,34 +36,7 @@ int	main( int argc, char **argv )
 	}
 	
 	Server server( port , argv[2] );
-	while (server.server_command()) {}; // loop until exit command
-
-
-	/**************test val *****************************/
-	// try{
-	// 	Server server(4242, "1234");
-	// 	Client client("michel", 1, server.new_ID(), true);
-	// 	Client client2("jean", 2, server.new_ID(), true);
-	// 	Channel channel("test", client);
-	// 	channel.add_user_by_admin(client2, client);
-	// 	channel.set_password("1234", client);
-	// 	std::cout << channel.get_password() << std::endl;
-	// 	server.add_channel(&channel);
-	// 	std::vector<Channel *> channels = server.get_channels();
-	// 	Channel channell = *channels[0];
-	// 	client.setAdmin(true);
-	// 	std::cout << "channel name " << channell.get_channel_name() << " channel pwd " << channell.get_password() << std::endl;
-	// 	parsing_command(argv[2], server.get_channels(), client);
-	// }
-	// catch (std::exception &e){
-	// 	std::cerr << BRed;
-	// 	std::cerr << e.what() << std::endl;
-	// 	std::cerr << Color_Off;
-	// }
-	/******************************************************/
-	// server.start();
-	// server.stop();
+	while (server.server_command()) {}; // loop until '/exit' command
 
 	return 0;
 }
-
